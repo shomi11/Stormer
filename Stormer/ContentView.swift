24415c0bfded0d76b16b22a0d5978d8cf01690ba
Milos <mm251134@gmail.com>
2021-05-29 14:11:46 +0200

    -Response models added

---
+++ Stormer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stormer
+//
+//  Created by Milos Malovic on 29.5.21..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stormer/StormerApp.swift
@@ -0,0 +1,19 @@
+//
+//  StormerApp.swift
+//  Stormer
+//
+//  Created by Milos Malovic on 29.5.21..
+//
+
+import SwiftUI
+
+@main
+struct StormerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+// open weather api key: 06b772226da20b9047cdcb341d815063
