147431838352f368d9dde51509fc2e33dfe5a74c
Milos <mm251134@gmail.com>
2021-05-29 15:39:18 +0200

    -Configuring Api service started

---
+++ Stormer/ApiService/ApiError.swift
@@ -0,0 +1,13 @@
+//
+//  ApiError.swift
+//  Stormer
+//
+//  Created by Milos Malovic on 29.5.21..
+//
+
+import Foundation
+
+enum ApiError {
+    case url
+    case notFound
+}
---
+++ Stormer/ApiService/Router.swift
@@ -0,0 +1,23 @@
+//
+//  Request.swift
+//  Stormer
+//
+//  Created by Milos Malovic on 29.5.21..
+//
+
+import Foundation
+
+enum Router {
+
+    static let apiKey = "06b772226da20b9047cdcb341d815063"
+    static var baseUrl = "api.openweathermap.org/data/2.5/find?"
+
+    case getForecasts(coordinated: Coord)
+
+
+
+//    func request() throws -> URLRequest {
+//
+//    }
+
+}
